0e44434e184e7ef66feece3df9e383a900703fc5
Brett <bwkaiser@gmail.com>
2018-03-26 12:49:42 -0500

    Added ActiveAdmin and SemanticUI Gems. Ran an install for ActiveAdmin

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 # My gems added after the fact
 gem 'semantic-ui-sass'
 gem 'activeadmin'
+gem 'devise'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
---
+++ db/migrate/20180326172814_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.integer :quantity
+      t.decimal :price, :scale => 2
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
