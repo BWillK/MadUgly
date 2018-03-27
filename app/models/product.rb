79e55c01d4ecf59d5d981a034ee50b24aac369ac
Brett <bwkaiser@gmail.com>
2018-03-27 16:37:05 -0500

    Setup a many to many via a join table between Products and Categories. Removed category column from Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
-  belongs_to :category
+  has_many :product_categories
+  has_many :categories, through: :product_categories
   mount_uploader :image, ImageUploader
+  validates :name, :description, presence: true
+  validates :quantity, :price, presence: true, numericality: true
 end
