6e7f6d00decbf691ed35a10ad1599fd402f83be7
Brett <bwkaiser@gmail.com>
2018-03-26 21:10:18 -0500

    Added image:string column to Products table for Carrierwave.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326204614) do
+ActiveRecord::Schema.define(version: 20180327020857) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20180326204614) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
