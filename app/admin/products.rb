d4d8f9caddd5631333f17446515d80efabf146c5
Brett <bwkaiser@gmail.com>
2018-03-27 19:14:53 -0500

    Modified products create/edit form to allow ProductCategories

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -12,6 +12,24 @@ ActiveAdmin.register Product do
 #   permitted
 # end
 
-  permit_params :name, :description, :quantity, :price, :active, :image, :category
+  permit_params :name, :description, :quantity, :price, :active, :image, :product_categories_ids
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs "Product" do
+      f.input :name
+      f.input :description
+      f.input :quantity
+      f.input :price
+      f.input :active
+      f.input :image
+      f.has_many :product_categories, allow_destroy: true do |n_f|
+        n_f.input :category
+      end
+    end
+
+    f.actions
+  end
 
 end
