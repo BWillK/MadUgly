fe241cb14d9e1ebcbfa5ab44d181408b91fa1b9a
Brett <bwkaiser@gmail.com>
2018-04-12 12:26:40 -0500

    Followed this guide to create a cart controller: https://jedrekdomanski.wordpress.com/2017/02/05/building-a-shopping-cart-in-ruby-on-rails-part-1/

---
+++ db/migrate/20180412172443_drop_tablename.rb
@@ -0,0 +1,16 @@
+class DropTablename < ActiveRecord::Migration[5.1]
+  def up
+    drop_table :order
+  end
+
+  def down
+    create_table :orders do |t|
+      t.references :customer, foreign_key: true
+      t.decimal :total, precision: 20, scale: 2
+      t.decimal :taxes, precision: 20, scale: 2
+
+      t.timestamps
+    end
+  end
+
+end
